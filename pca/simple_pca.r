2f7b6fe7d52dbcedc42ac79c698d890d5f1e6b75
Austin G. Davis-Richardson <harekrishna@gmail.com>
2011-12-21 14:17:13 -0500

    might as well have a directory for pca scripts

--- pca/simple_pca.r
+++ pca/simple_pca.r
@@ -7,11 +7,13 @@ args <- commandArgs(TRUE)
 data_table = args[1]
 
 
-mydata = read.table(file=data_table, header=TRUE, row.names=1, sep="\t")
+mydata = read.table(file=data_table, header=TRUE, row.names=1, sep=",")
 
 mydata.pca = prcomp (mydata, retx=TRUE, center=TRUE, scale.=TRUE)
 
 mydata.pca
+
+length_factor = as.integer(Sys.getenv('lf'))
 
 sd = mydata.pca$sdev
 loadings = mydata.pca$rotation
@@ -22,12 +24,16 @@ quartz(height=7, width=7) > plot(scores[,1], scores[,2], xlab="PCA 1", ylab="PCA
   type="n", xlim=c(min(scores[,1:2]), max(scores[,1:2])),
   ylim=c(min(scores[,1:2]), max(scores[,1:2])))
 
-arrows(0,0,loadings[,1]*20,loadings[,2]*20, length=0.1,
+arrows(0,0,loadings[,1]*10,loadings[,2]*length_factor, length=0.1,
 angle=20, col="red") 
 
 # note that this scaling factor *n may need to be changed, # depending on the data set
 
-text(loadings[,1]*20*1.2,loadings[,2]*20*1.2, rownames(loadings), col="red", cex=0.7)
+text(loadings[,1]*length_factor*1.2,loadings[,2]*length_factor*1.2, rownames(loadings), col="red", cex=0.7)
 # 1.2 scaling insures that labels are plotted just beyond # the arrows
 
 text(scores[,1],scores[,2], rownames(scores), col="blue", cex=0.7)
+
+
+dev.copy(pdf,'myplot.pdf')
+dev.off()
